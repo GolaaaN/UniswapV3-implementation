c715b5eeab5cce41df71a11edeb20409e0173f6b
ilai <ilaigolan100@gmail.com>
2023-04-09 18:08:34 +0300

    Created two new tokens

---
+++ src/RedToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.8.0) (token/ERC20/ERC20.sol)
+pragma solidity ^0.8.0;
+
+import "vendors/contracts/token/ERC20/ERC20.sol";
+
+contract RedToken is ERC20{
+    address private _dex;
+    constructor(address dexInstance, string memory name, string memory symbol, uint initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+        _dex = dexInstance;
+    }
+}
