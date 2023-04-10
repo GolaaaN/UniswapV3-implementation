9b9f78051ee88cb69062d70a077e41a84dedd180
ilai <ilaigolan100@gmail.com>
2023-04-10 13:24:39 +0300

    added uniswap core files and made py file for calc

--- test/uniswap.t.sol
+++ test/uniswap.t.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 
-contract UniswapTest is Test {
+contract UniswapTest is Test{
+    
+    
+    function setUp() public {
+        
+    }
+
     function testIlai() public{
         assertEq(false, true);
     }
