bfab38d21011d842a6a444436e0e21c9ac06f2da
ilai <ilaigolan100@gmail.com>
2023-04-13 14:02:11 +0300

    created swap tests

--- src/BlueToken.sol
+++ src/BlueToken.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.8.0) (token/ERC20/ERC20.sol)
-pragma solidity ^0.8.0;
+pragma solidity ^0.7.6;
 
 import "openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract BlueToken is ERC20{
-    address private _dex;
-    constructor(address dexInstance, string memory name, string memory symbol, uint initialSupply) ERC20(name, symbol) {
+contract BlueToken is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint initialSupply
+    ) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
-        _dex = dexInstance;
     }
 }
--- test/uniswap.t.sol
+++ test/uniswap.t.sol
@@ -1,17 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.7.6;
 
 import "forge-std/Test.sol";
+import "../src/BlueToken.sol";
+import "../src/RedToken.sol";
+import "../uniswap-v3-core/contracts/UniswapV3Factory.sol"; //factory;
+import "../uniswap-v3-core/contracts/UniswapV3Pool.sol"; //pool
+import "../uniswap-v3-core/contracts/interfaces/callback/IUniswapV3MintCallback.sol";
+import "../uniswap-v3-core/contracts/interfaces/callback/IUniswapV3SwapCallback.sol";
 
+contract UniswapTest is Test, IUniswapV3MintCallback {
+    address public redToken;
+    address public blueToken;
+    UniswapV3Factory public factory;
+    UniswapV3Pool testPool;
+    uint24 public constant fee = 3000;
+    uint24 tickLower;
+    uint24 tickUpper;
 
-contract UniswapTest is Test{
-    
-    
     function setUp() public {
-        
+        //create two tokens
+        redToken = new RedToken("RED", "RED", 10);
+        blueToken = new BlueToken("BLUE", "BLU", 10);
+
+        //create factory
+        factory = new UniswapV3Factory();
+        testPool = factory.createPool(redToken, blueToken, fee);
     }
 
-    function testIlai() public{
-        assertEq(false, true);
+    function testSwap() public {
+        //after the deployment of the factory we create pool with our tokens and 0.3% fees
+
+        testPool.mint(msg.sender, tickLower, tickUpper, 100, " ");
+
+        testPool.swap();
+    }
+
+    function uniswapV3MintCallback(
+        uint256 amount0Owed,
+        uint256 amount1Owed,
+        bytes calldata data
+    ) external override {
+        //approve the pool to spend our tokens.
+        token0.approve(testPool, 10);
+        token1.approve(testPool, 10);
+        //transfer tokens to the pool adter init the liq.
+        token0.transfer(testPool, 10);
+        token1.transfer(testPool, 10);
     }
 }
